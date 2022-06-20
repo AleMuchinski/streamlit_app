04f05cd765105580bf73cb3633d23feed8ab9e9a
Alexandre Patrick Ganz Muchinski <48166611+AleMuchinski@users.noreply.github.com>
2022-06-20 14:47:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,5 +15,5 @@ streamlit.dataframe(my_fruit_list)
 
 
 
-streamlit.multiselect('Pick some fruits:',list(my_fruit_list.index))
+streamlit.multiselect('Pick some fruits:',list(my_fruit_list.index),['Avocado','Strawberries'])
 streamlit.dataframe(my_fruit_list)
